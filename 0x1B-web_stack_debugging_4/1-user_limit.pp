eb6baaa84d940499210eadc15757658d5032f699
talemwaraymond <talemwaray@gmail.com>
2024-05-15 12:24:16 +0000

    1-user_limit.pp

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,13 @@
+#Enable the user holberton to login and open files without errors
+
+#this will increase hard file limit for holberton user
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf'
+  path    => '/usr/local/bin/:/bin/'
+}
+
+#this will increase the soft file limit for user holberton
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf'
+  path    => '/usr/local/bin/:/bin/'
+}
